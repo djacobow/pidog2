98d227117511bec3e97862d73a7cd7b4faf248cf
Dave Jacobowitz <david.jacobowitz@gmail.com>
2018-09-26 21:31:03 -0700

    just use image that is in repo

--- rpi_lib/flashing/reflash.sh
+++ rpi_lib/flashing/reflash.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-curl https://github.com/djacobow/pidog2/raw/master/firmware_images/pidog2.ino.hex
-
 sudo avrdude \
     -C avrdude-gpio.conf -c rpi_pidog_gpio \
     -p t84 \
     -U lfuse:w:0xe2:m \
     -U hfuse:w:0xdf:m \
     -U efuse:w:0xff:m \
-    -U pidog2.ino.hex \
+    -U ../../firmware_images/pidog2.ino.hex \
     -v
+
+
 
 
